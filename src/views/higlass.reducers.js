491fcbd9560ef9c141943034c2617cb42e3b6e8d
Fritz Lekschas <service@lekschas.de>
2017-01-27 18:54:31 -0500

    Fix reducer

--- src/views/higlass.reducers.js
+++ src/views/higlass.reducers.js
@@ -8,8 +8,7 @@ import deepClone from 'utils/deep-clone';
 export function config (state = { ...CONFIG }, action) {
   switch (action.type) {
     case UPDATE_HGL_CONFIG:
-      console.log('DEEP CLONE CONFIG for HGL');
-      return { ...state, config: deepClone(action.payload.config) };
+      return { ...state, ...deepClone(action.payload.config) };
 
     default:
       return state;
