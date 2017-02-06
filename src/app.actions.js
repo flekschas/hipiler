e6a8065e3faa212d32952bdc239e76b89c5bf2da
Fritz Lekschas <service@lekschas.de>
2017-02-05 21:17:44 -0500

    More eslint fixes

--- src/app.actions.js
+++ src/app.actions.js
@@ -1,11 +1,9 @@
-export const UPDATE_CONFIG = 'UPDATE_CONFIG';
-
 import { updateConfig as updateHglConfig } from 'components/higlass/actions';
 import { updateConfig as updateFgmConfig } from 'components/fragments/actions';
 
-export const updateConfigs = (config) => {
-  return function (dispath) {
-    dispath(updateHglConfig(config.hgl));
-    dispath(updateFgmConfig(config.fgm));
-  };
+export const UPDATE_CONFIG = 'UPDATE_CONFIG';
+
+export const updateConfigs = config => (dispath) => {
+  dispath(updateHglConfig(config.hgl));
+  dispath(updateFgmConfig(config.fgm));
 };
