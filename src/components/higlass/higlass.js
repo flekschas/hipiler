77c3fbe32e616dc5f421b9b9afba10642dd54090
Fritz Lekschas <service@lekschas.de>
2017-02-08 16:30:00 -0500

    Handle server downtimes

--- src/components/higlass/higlass.js
+++ src/components/higlass/higlass.js
@@ -7,6 +7,7 @@ import States from 'services/states';
 // Utils etc.
 import $ from 'utils/dom-el';
 import debounce from 'utils/debounce';
+import ping from 'utils/ping';
 import { createHgComponent as hg } from 'hglib';  // eslint-disable-line
 import { requestNextAnimationFrame } from 'utils/request-animation-frame';
 
@@ -37,6 +38,38 @@ export class Higlass {
     setTimeout(() => { this.isLoading = false; }, 150);
   }
 
+
+  /* ----------------------- Getter / Setter Variables ---------------------- */
+
+  get isErrored () {
+    return this._isErrored;
+  }
+
+  set isErrored (value) {
+    if (!value) {
+      this.errorMsg = undefined;
+    }
+
+    this._isErrored = value;
+  }
+
+
+  /* ---------------------------- Custom Methods ---------------------------- */
+
+  areServersAvailable (config) {
+    let servers;
+
+    try {
+      // This needs refactoring but we need to wait until Higlass's config has
+      // been refactored.
+      servers = [config.views[0].tracks.center[0].contents[0].server];
+    } catch (e) {
+      return Promise.reject(Error('Broken config'));
+    }
+
+    return Promise.all(servers.map(server => ping(server)));
+  }
+
   checkColumns (newColumns) {
     if (this.columns !== newColumns) {
       this.columns = newColumns;
@@ -46,6 +79,11 @@ export class Higlass {
         $(window).dispatch('resize', 'HTMLEvents');
       });
     }
+  }
+
+  hasErrored (errorMsg) {
+    this.errorMsg = errorMsg;
+    this.isErrored = true;
   }
 
   update () {
@@ -60,11 +98,18 @@ export class Higlass {
   updateConfig (newConfig) {
     if (this.higlass.config !== newConfig) {
       this.higlass.config = newConfig;
+
       this.renderDb(this.higlass.config);
     }
   }
 
   render (config) {
-    hg(this.plotEl, config, OPTIONS, (api) => { this.api = api; });
+    this.areServersAvailable(config)
+      .then(() => {
+        hg(this.plotEl, config, OPTIONS, (api) => { this.api = api; });
+      })
+      .catch((error) => {
+        this.hasErrored('Server not available');
+      });
   }
 }
