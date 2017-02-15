f65c3256103b6eacc88a711e0681d61a0513365f
Fritz Lekschas <service@lekschas.de>
2017-02-14 20:53:37 -0500

    Add hex colors

---
+++ src/configs/colors.js
@@ -0,0 +1,38 @@
+export const BLACK = 0x000000;
+export const GRAY = 0x999999;
+export const WHITE = 0xffffff;
+
+export const GRAY_LIGHT = 0xbfbfbf;
+export const GRAY_LIGHTER = 0xd9d9d9;
+export const GRAY_LIGHTEST = 0xe5e5e5;
+export const GRAY_DARK = 0x666666;
+export const GRAY_DARKER = 0x444444;
+export const GRAY_DARKEST = 0x222222;
+
+export const GREEN = 0x40bf00;
+export const ORANGE = 0xff5500;
+export const RED = 0xf60029;
+export const YELLOW = 0xffcc00;
+export const BLUE = 0x4e40ff;
+
+export const PRIMARY = ORANGE;
+export const SECONDARY = BLUE;
+
+export default {
+  BLACK,
+  GRAY,
+  WHITE,
+  GRAY_LIGHT,
+  GRAY_LIGHTER,
+  GRAY_LIGHTEST,
+  GRAY_DARK,
+  GRAY_DARKER,
+  GRAY_DARKEST,
+  GREEN,
+  ORANGE,
+  RED,
+  YELLOW,
+  BLUE,
+  PRIMARY,
+  SECONDARY
+};
