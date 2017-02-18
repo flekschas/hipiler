52c3de9314234ed676ef3dbbe5d70beac23a2cab
Fritz Lekschas <service@lekschas.de>
2017-02-18 17:08:42 -0500

    Add a field for fragment related measures

--- src/components/fragments/matrix.js
+++ src/components/fragments/matrix.js
@@ -1,8 +1,9 @@
 export default class Matrix {
-  constructor (id, matrix, locus) {
+  constructor (id, matrix, locus, measures) {
     this.id = id;
     this.matrix = matrix;
     this.locus = locus;
+    this.measures = measures;
 
     this.dim = matrix.length;
   }
