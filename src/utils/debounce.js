7767902ce466e26182b84f020040a3aad5f80a3e
Fritz Lekschas <service@lekschas.de>
2017-02-20 21:32:17 -0500

    Update docs

--- src/utils/debounce.js
+++ src/utils/debounce.js
@@ -1,22 +1,14 @@
 /**
- * Debounce a function call
+ * Debounce a function call.
  *
  * @description
  * Function calls are delayed by `wait` milliseconds and only one out of
  * multiple function calls is executed.
  *
- * @note
- * Once webpack 2 with tree-shaking is supported I'd advocate to use lodash's
- * debounce method.
- *
- * @method  debounce
- * @author  Fritz Lekschas
- * @date    2017-01-14
- * @param   {Functiom}   func       Function to be debounced
- * @param   {Number}     wait       Number of milliseconds to debounce the
- *   function call.
- * @param   {Boolean}    immediate  If `true` function is not debounced.
- * @return  {Functiomn}             Debounced function.
+ * @param {functiom} func - Function to be debounced
+ * @param {number} wait - Number of milliseconds to debounce the function call.
+ * @param {boolean} immediate - If `true` function is not debounced.
+ * @return {functiomn} Debounced function.
  */
 export default function debounce (func, wait, immediate) {
   let timeout;
