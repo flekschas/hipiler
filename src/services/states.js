f8049be3199724d13b7262a72f1147cac8f5f04a
Fritz Lekschas <service@lekschas.de>
2017-02-27 19:24:26 -0500

    Avoid deepFreeze in normal use as it causes trouble with higlass or some other third party lib

--- src/services/states.js
+++ src/services/states.js
@@ -3,7 +3,7 @@ import localForage from 'localForage';
 import { applyMiddleware, compose, createStore } from 'redux';
 import { autoRehydrate, persistStore, purgeStoredState } from 'redux-persist';
 import thunk from 'redux-thunk';
-import freeze from 'redux-freeze';
+// import freeze from 'redux-freeze';
 import undoable, { ActionCreators } from 'redux-undo';
 
 import appReducer from 'app-reducer';
@@ -22,7 +22,7 @@ export default class States {
       compose(
         autoRehydrate(),
         applyMiddleware(thunk),
-        applyMiddleware(freeze)
+        // applyMiddleware(freeze)
       )
     );
 
