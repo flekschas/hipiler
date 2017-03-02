77427b97e8a0bf045f15ba3893cc91e288dca625
Fritz Lekschas <service@lekschas.de>
2017-03-01 21:24:35 -0500

    Implement redux actions. Extend bottom bar

--- src/components/higlass/higlass.js
+++ src/components/higlass/higlass.js
@@ -8,8 +8,18 @@ import States from 'services/states';
 import $ from 'utils/dom-el';
 import debounce from 'utils/debounce';
 import ping from 'utils/ping';
-import { createHgComponent as hg } from 'hglib';  // eslint-disable-line
+import { createHgComponent as hg } from 'hglib';
 import { requestNextAnimationFrame } from 'utils/request-animation-frame';
+import {
+  setGrayscale,
+  setFragmentsHighlight,
+  setInteractions
+} from 'components/higlass/higlass-actions';
+import {
+  GRAYSCALE_COLORS
+} from 'components/higlass/higlass-defaults';
+
+import deepClone from 'utils/deep-clone';
 
 const logger = LogManager.getLogger('higlass');
 
@@ -23,8 +33,6 @@ export class Higlass {
     // Link the Redux store
     this.store = states.store;
     this.store.subscribe(this.update.bind(this));
-
-    this.higlass = {};
 
     this.renderDb = debounce(this.render.bind(this), 50);
     this.checkColumnsDb = debounce(this.checkColumns.bind(this), 150);
@@ -81,26 +89,158 @@ export class Higlass {
     }
   }
 
+  /**
+   * Extract loci in modified BEDPE format for higlass.
+   *
+   * @param {object} fgmConfig - Fragment config.
+   * @return {array} List of loci.
+   */
+  extractLoci (fgmConfig) {
+    const dataIdxChrom1 = fgmConfig.fragmentsHeader.indexOf('chrom1');
+    const dataIdxStart1 = fgmConfig.fragmentsHeader.indexOf('start1');
+    const dataIdxEnd1 = fgmConfig.fragmentsHeader.indexOf('end1');
+    const dataIdxChrom2 = fgmConfig.fragmentsHeader.indexOf('chrom2');
+    const dataIdxStart2 = fgmConfig.fragmentsHeader.indexOf('start2');
+    const dataIdxEnd2 = fgmConfig.fragmentsHeader.indexOf('end2');
+
+    return fgmConfig.fragments.map(fragment => [
+      `chr${fragment[dataIdxChrom1]}`,
+      fragment[dataIdxStart1],
+      fragment[dataIdxEnd1],
+      `chr${fragment[dataIdxChrom2]}`,
+      fragment[dataIdxStart2],
+      fragment[dataIdxEnd2],
+      'rgba(255, 85, 0, 0.8)'
+    ]);
+  }
+
+  /**
+   * Handles changes of highlighting fragments.
+   */
+  fragmentsHighlightChangeHandler () {
+    this.store.dispatch(setFragmentsHighlight(!this.fragmentsHighlight));
+
+    return true;
+  }
+
+  /**
+   * Handles changes of grayscale.
+   */
+  grayscaleChangeHandler () {
+    this.store.dispatch(setGrayscale(!this.grayscale));
+
+    return true;
+  }
+
+  /**
+   * Helper method for showing an error.
+   *
+   * @param {string} errorMsg - Error message to be displayed.
+   */
   hasErrored (errorMsg) {
     this.errorMsg = errorMsg;
     this.isErrored = true;
   }
 
+  /**
+   * Handles changes of interaction.
+   */
+  interactionsChangeHandler () {
+    this.store.dispatch(setInteractions(!this.interactions));
+
+    return true;
+  }
+
   update () {
     try {
-      this.checkColumnsDb(this.store.getState().present.decompose.columns);
-      this.updateConfig(this.store.getState().present.decompose.higlass.config);
+      const state = this.store.getState().present.decompose;
+
+      const update = {};
+
+      this.checkColumnsDb(state.columns, update);
+      this.updateConfig(state.higlass.config, update);
+      this.updateInteractions(
+        state.higlass.interactions, update, update.render
+      );
+      this.updateGrayscale(state.higlass.grayscale, update, update.render);
+      this.updateFragmentsHighlight(
+        state.higlass.fragmentsHighlight,
+        state.fragments.config,
+        update,
+        update.render
+      );
+
+      if (update.render) {
+        this.renderDb(this.config);
+      }
     } catch (e) {
       logger.error('State is invalid', e);
     }
   }
 
-  updateConfig (newConfig) {
-    if (this.higlass.config !== newConfig) {
-      this.higlass.config = newConfig;
+  updateConfig (config, update) {
+    if (this.config === config) { return; }
 
-      this.renderDb(this.higlass.config);
+    this.config = deepClone(config);
+
+    this.originalColoring = config
+      .views[0].tracks.center[0].contents[0].options.colorRange.slice();
+
+    update.render = true;
+  }
+
+  updateInteractions (interactions, update, force) {
+    if (this.interactions === interactions && !force) { return; }
+
+    this.interactions = interactions;
+
+    this.config.zoomFixed = !this.interactions;
+
+    update.render = true;
+  }
+
+  updateGrayscale (grayscale, update, force) {
+    if (this.grayscale === grayscale && !force) { return; }
+
+    this.grayscale = grayscale;
+
+    if (this.grayscale) {
+      this.config.views[0].tracks.center[0].contents[0].options.colorRange =
+        GRAYSCALE_COLORS;
+    } else {
+      this.config.views[0].tracks.center[0].contents[0].options.colorRange =
+        this.originalColoring;
     }
+
+    update.render = true;
+  }
+
+  updateFragmentsHighlight (fgmHighlight, fgmConfig, update, force) {
+    if (this.fgmHighlight === fgmHighlight && !force) { return; }
+
+    this.fragmentsHighlight = fgmHighlight;
+
+    if (this.config.views[0].tracks.center.length > 1) {
+      this.config.views[0].tracks.center = [
+        this.config.views[0].tracks.center[0]
+      ];
+    }
+
+    if (this.fragmentsHighlight) {
+      const loci = this.extractLoci(fgmConfig);
+
+      this.config.views[0].tracks.center.push({
+        type: '2d-chromosome-annotations',
+        chromInfoPath: '//s3.amazonaws.com/pkerp/data/hg19/chromSizes.tsv',
+        options: {
+          minRectWidth: 2,
+          minRectHeight: 2,
+          regions: loci
+        }
+      });
+    }
+
+    update.render = true;
   }
 
   render (config) {
