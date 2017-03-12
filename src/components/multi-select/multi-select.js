71fd48355775e3af6691655090298d66d7411063
Fritz Lekschas <service@lekschas.de>
2017-03-12 13:20:45 -0400

    Various bug fixes and show selected options

--- src/components/multi-select/multi-select.js
+++ src/components/multi-select/multi-select.js
@@ -11,6 +11,7 @@ import States from 'services/states';
 
 import { EVENT_BASE_NAME } from 'components/multi-select/multi-select-defaults';
 
+import arraysEqual from 'utils/arrays-equal';
 import hasParent from 'utils/has-parent';
 import mod from 'utils/mod';
 import queryObj from 'utils/query-obj';
@@ -53,7 +54,6 @@ export class MultiSelect {
 
   get selectableOptions () {
     return this.options
-      .filter(option => !option.isSelected)
       .filter(option => (
         this.inputEl.value.length ?
           option.name.toLowerCase().indexOf(
@@ -148,7 +148,6 @@ export class MultiSelect {
     const removedOption = this.selectedOptions.pop();
 
     if (removedOption) {
-      this.selectedOptionsIdx[removedOption.id] = undefined;
       this.options
         .filter(option => option.id === removedOption.id)
         .forEach((option) => { option.isSelected = false; });
@@ -157,50 +156,50 @@ export class MultiSelect {
     this.publish();
   }
 
-  removeSelectedOption (optionRemove) {
-    const index = this.selectedOptions.findIndex(
-      option => option.id === optionRemove.id
-    );
-
-    if (index >= 0) {
-      const removedOption = this.selectedOptions.splice(index, 1);
-
-      this.selectedOptionsIdx[removedOption.id] = undefined;
-
-      this.options
-        .filter(option => option.id === optionRemove.id)
-        .forEach((option) => { option.isSelected = false; });
-    }
-
-    this.publish();
-  }
-
   select (option) {
-    if (typeof this.selectedOptionsIdx[option.id] === 'undefined') {
+    if (!option.isSelected) {
       this.selectedOptions.push(option);
       this.focusedOptionId = -1;
       option.isSelected = true;
       option.isFocus = false;
 
-      this.selectedOptionsIdx[option.id] = this.selectedOptions.length - 1;
-
       this.publish();
+    } else {
+      this.unselect(option);
     }
   }
 
   selectBatch (options) {
     options.forEach((option) => {
-      if (typeof this.selectedOptionsIdx[option.id] === 'undefined') {
+      if (!option.isSelected) {
         this.selectedOptions.push(option);
         this.focusedOptionId = -1;
         option.isSelected = true;
         option.isFocus = false;
 
-        this.selectedOptionsIdx[option.id] = this.selectedOptions.length - 1;
-
         this.publish();
       }
     });
+  }
+
+  unselect (optionRemove) {
+    if (!optionRemove.isSelected) {
+      return;
+    }
+
+    this.options
+      .filter(option => option.id === optionRemove.id)
+      .forEach((option) => {
+        option.isSelected = false;
+      });
+
+    const index = this.selectedOptions.indexOf(optionRemove);
+
+    if (index >= 0) {
+      this.selectedOptions.splice(index, 1);
+    }
+
+    this.publish();
   }
 
   update () {
@@ -208,10 +207,13 @@ export class MultiSelect {
       this.store.getState().present, this.stateQuery
     );
 
+    const currentSelection = this.selectedOptions.map(option => option.id);
+
     if (
       !selectedOptionsState ||
       this.options.length === 0 ||
-      this.selectedOptionsState === selectedOptionsState
+      this.selectedOptionsState === selectedOptionsState ||
+      arraysEqual(currentSelection, selectedOptionsState)
     ) {
       return;
     }
@@ -220,23 +222,16 @@ export class MultiSelect {
 
     const newSelection = [];
 
-    const selectedOptionsStack = selectedOptionsState.slice();
+    selectedOptionsState.forEach((selectedOptionId) => {
+      this.options
+        .filter(option => option.id === selectedOptionId)
+        .forEach((option) => {
+          option.isSelected = true;
+          option.isFocus = false;
+          newSelection.push(option);
+        });
+    });
 
-    if (selectedOptionsStack.length) {
-      for (let i = 0; i < this.options.length; i++) {
-        const pos = selectedOptionsStack.indexOf(this.options[i].id);
-        if (pos >= 0) {
-          this.options[i].isSelected = true;
-          this.options[i].isFocus = false;
-          newSelection.push(this.options[i]);
-          selectedOptionsStack.splice(pos, 1);
-
-          if (!selectedOptionsStack.length) {
-            break;
-          }
-        }
-      }
-    }
     this.selectedOptions = newSelection;
   }
 }
