59f5c1475bc14ad99bd27fe2e0c1329438c37ccb
Fritz Lekschas <service@lekschas.de>
2017-03-20 23:10:46 -0400

    Implement basic dialog

---
+++ src/components/dialog/dialog.js
@@ -0,0 +1,25 @@
+// Aurelia
+import {
+  bindable,
+  bindingMode
+} from 'aurelia-framework';
+
+export class Dialog {
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) deferred = {};
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) isOpen = false;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) message = '';
+
+  cancel () {
+    if (this.deferred && this.deferred.reject) {
+      this.deferred.reject();
+      this.isOpen = false;
+    }
+  }
+
+  okay () {
+    if (this.deferred && this.deferred.resolve) {
+      this.deferred.resolve();
+      this.isOpen = false;
+    }
+  }
+}
