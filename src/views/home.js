e54122ea26b4bb0a2ac8d652e4f00196c4f47e0b
Fritz Lekschas <service@lekschas.de>
2017-04-14 10:58:48 -0400

    Design home page

--- src/views/home.js
+++ src/views/home.js
@@ -1,17 +1,65 @@
 // Aurelia
-import { inject } from 'aurelia-framework';
+import { inject, LogManager } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
 
 // Injectable
 import States from 'services/states';
 
+// Utils
+import { updateConfigs } from 'app-actions';
+import readJsonFile from 'utils/read-json-file';
+import validateConfig from 'utils/validate-config';
 
-@inject(States)
+const logger = LogManager.getLogger('home');
+
+
+@inject(Router, States)
 export class Home {
-  constructor (states) {
+  inputConfigFile;
+
+  constructor (router, states) {
+    this.router = router;
+
     this.store = states.store;
     this.store.subscribe(this.update.bind(this));
+  }
 
-    this.update();
+  attached () {
+    this.selectConfigButton.addEventListener(
+      'click', this.selectConfig.bind(this)
+    );
+  }
+
+  selectedConfigChanged () {
+    let results;
+
+    try {
+      results = readJsonFile(this.selectedConfig[0]);
+    } catch (error) {
+      logger.error(error);
+    }
+
+    results
+      .then(json => this.setState(json))
+      .catch(error => logger.error(error));
+  }
+
+  selectConfig (event) {
+    if (event.artificial) { return; }
+
+    const newEvent = new MouseEvent(event.type, event);
+    newEvent.artificial = true;
+
+    this.inputConfigFile.dispatchEvent(newEvent);
+  }
+
+  setState (config) {
+    if (validateConfig(config.fgm, config.hgl)) {
+      this.store.dispatch(updateConfigs(config));
+      this.router.navigateToRoute('decompose');
+    } else {
+      this.showGlobalError('Corrupted Config File', 3000);
+    }
   }
 
   update () {}
