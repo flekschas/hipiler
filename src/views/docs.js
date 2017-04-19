e5e0b329bc2f66febad7402629a322156412be75
Fritz Lekschas <service@lekschas.de>
2017-04-19 19:14:16 -0400

    Append wiki HTML and add style sheet for docs

--- src/views/docs.js
+++ src/views/docs.js
@@ -4,12 +4,17 @@ import { inject } from 'aurelia-framework';
 // Injectable
 import States from 'services/states';
 
+// Docs
+import wiki from 'text!../../assets/wiki/wiki.html';  // eslint-disable-line import/no-webpack-loader-syntax
+
 
 @inject(States)
 export class Docs {
   constructor (states) {
     this.store = states.store;
     this.store.subscribe(this.update.bind(this));
+
+    this.docs = wiki;
 
     this.update();
   }
