7ae4a1f4727b71311672469cbd8dbe50dca8902c
Fritz Lekschas <service@lekschas.de>
2017-04-19 10:59:13 -0400

    Update dialog appearance and enable keyboard shortcuts

--- src/components/dialog/dialog.js
+++ src/components/dialog/dialog.js
@@ -1,14 +1,27 @@
 // Aurelia
 import {
   bindable,
-  bindingMode
+  bindingMode,
+  inject
 } from 'aurelia-framework';
 
+import { EventAggregator } from 'aurelia-event-aggregator';
+
+@inject(EventAggregator)
 export class Dialog {
   @bindable({ defaultBindingMode: bindingMode.twoWay }) deferred = {};
   @bindable({ defaultBindingMode: bindingMode.twoWay }) isOpen = false;
   @bindable({ defaultBindingMode: bindingMode.oneWay }) message = '';
 
+  constructor (event) {
+    this.event = event;
+
+    this.initEventListeners();
+  }
+
+  /**
+   * Cancel dialog. This will reject the promise and clode the dialog window.
+   */
   cancel () {
     if (this.deferred && this.deferred.reject) {
       this.deferred.reject(Error());
@@ -16,6 +29,37 @@ export class Dialog {
     }
   }
 
+  /**
+   * Initializae event listeners.
+   */
+  initEventListeners () {
+    this.event.subscribe('app.keyUp', this.keyUpHandler.bind(this));
+  }
+
+  /**
+   * Handle key up events and delegate the tasks.
+   *
+   * @param {object} event - Event object.
+   */
+  keyUpHandler (event) {
+    switch (event.keyCode) {
+      case 13:  // Enter
+        this.okay();
+        break;
+
+      case 27:  // ESC
+        this.cancel();
+        break;
+
+      default:
+        // Nothing
+        break;
+    }
+  }
+
+  /**
+   * Accept dialog. This will resolve the promise and close the dialog.
+   */
   okay () {
     if (this.deferred && this.deferred.resolve) {
       this.deferred.resolve();
