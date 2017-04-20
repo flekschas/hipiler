b46c315eab726644f299a572b76b8f3302a98e9d
Fritz Lekschas <service@lekschas.de>
2017-04-20 12:39:56 -0400

    Make sidebar sticky

--- src/views/docs.js
+++ src/views/docs.js
@@ -1,23 +1,43 @@
 // Aurelia
 import { inject, InlineViewStrategy } from 'aurelia-framework';
-
-// Injectable
-import States from 'services/states';
+import { EventAggregator } from 'aurelia-event-aggregator';
 
 // Docs
+import sidebar from 'text!../../assets/wiki/sidebar.html';  // eslint-disable-line import/no-webpack-loader-syntax
 import wiki from 'text!../../assets/wiki/wiki.html';  // eslint-disable-line import/no-webpack-loader-syntax
 
 
-@inject(States)
+@inject(EventAggregator)
 export class Docs {
-  constructor (states) {
-    this.store = states.store;
-    this.store.subscribe(this.update.bind(this));
+  constructor (event) {
+    this.event = event;
 
     this.docs = new InlineViewStrategy(wiki);
 
-    this.update();
+    this.sidebar = new InlineViewStrategy(sidebar);
+    this.sidebarCss = {};
   }
 
-  update () {}
+  attached () {
+    this.sidebarOffsetTop = this.sidebarEl.getBoundingClientRect().top -
+      document.body.getBoundingClientRect().top;
+
+    this.initEventListeners();
+  }
+
+  adjustSidebarPos (event) {
+    this.sidebarMarginTop = Math.abs(
+      this.sidebarOffsetTop - this.sidebarEl.getBoundingClientRect().top
+    );
+
+    this.sidebarMarginTop = (this.sidebarMarginTop - 48) < 0 ? 0 : this.sidebarMarginTop - 48;
+
+    this.sidebarCss = {
+      'padding-top': `${this.sidebarMarginTop}px`
+    };
+  }
+
+  initEventListeners () {
+    this.event.subscribe('app.scroll', this.adjustSidebarPos.bind(this));
+  }
 }
