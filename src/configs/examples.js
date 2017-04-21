5b2852c0701caf5316b1a3922a7bc22de9ffd2e2
Fritz Lekschas <service@lekschas.de>
2017-04-21 17:59:35 -0400

    Update examples to always pull the latest code

--- src/configs/examples.js
+++ src/configs/examples.js
@@ -1,6 +1,6 @@
 export const rao2014Gm12878Mbol1kbMresChr22Loops = {
   name: 'Rao et al. (2014) GM12878 Mbol 1KB MRes Chr22 Loops',
-  url: 'https://rawgit.com/flekschas/8b0163f25fd4ffb067aaba2a595da447/raw/7404fce9de881ff14ccacc276b9c65a473ee7d98/rao-2014-gm12878-mbol-1kb-mres-chr22-loops.json'
+  url: 'https://rawgit.com/flekschas/8b0163f25fd4ffb067aaba2a595da447/raw/rao-2014-gm12878-mbol-1kb-mres-chr22-loops.json'
 };
 
 export default [
