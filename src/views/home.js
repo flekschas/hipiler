dc12a16eaf3599b4b1a98944306ee1d4ca8f0a9d
Fritz Lekschas <service@lekschas.de>
2017-08-17 17:16:12 -0400

    Remove logger

--- src/views/home.js
+++ src/views/home.js
@@ -60,8 +60,6 @@ export class Home {
     ) {
       this.queryParams = queryParams;
     }
-
-    console.log(this.queryParams);
   }
 
   /* ---------------------------- Class methods ----------------------------- */
