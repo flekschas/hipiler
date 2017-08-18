8b9292673e467a82fd401f33745d77dccbad41de
Fritz Lekschas <service@lekschas.de>
2017-08-17 22:02:16 -0400

    Update to get a dummy test running

--- aurelia_project/tasks/test.js
+++ aurelia_project/tasks/test.js
@@ -1,7 +1,7 @@
-import {Server as Karma} from 'karma';
-import {CLIOptions} from 'aurelia-cli';
+import { Server as Karma } from 'karma';
+import { CLIOptions } from 'aurelia-cli';
 
-export function unit(done) {
+export function unit (done) {
   new Karma({
     configFile: __dirname + '/../../karma.conf.js',
     singleRun: !CLIOptions.hasFlag('watch')
--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -1,7 +1,8 @@
-import {App} from '../../src/app';
+// import App from '../../src/app';
 
-describe('the app', () => {
-  it('says hello', () => {
-    expect(new App().message).toBe('Hello World!');
+
+describe('Dummy', () => {
+  it('test', () => {
+    expect(true).toBe(true);
   });
 });
