ffb1bfd3f0595c794a03b4385d56d1be45dd34f3
Fritz Lekschas <service@lekschas.de>
2019-12-05 16:28:26 -0500

    Fix examples

--- src/configs/examples.js
+++ src/configs/examples.js
@@ -1,25 +1,25 @@
 export const rao2014Gm12878Mbol1kbMresChr22Loops = {
   name: 'Loops in chromosome 22 of GM12878',
   data: 'Rao et al. (2014) GM12878 Mbol 1kb',
-  url: 'https://rawgit.com/flekschas/8b0163f25fd4ffb067aaba2a595da447/raw/rao-2014-gm12878-mbol-1kb-mres-chr22-loops.json'
+  url: 'https://gist.githubusercontent.com/flekschas/8b0163f25fd4ffb067aaba2a595da447/raw/12316079e1adb63450ad38e21fdb51950440e917/rao-2014-gm12878-mbol-1kb-mres-chr22-loops-hipiler-v140.json'
 };
 
 export const rao2014Gm12878Mbol1kbMresChr4Tads = {
   name: 'TADs in chromosome 4 of GM12878',
   data: 'Rao et al. (2014) GM12878 Mbol 1kb',
-  url: 'https://rawgit.com/flekschas/37b6293b82b5b3e5fb56de9827100797/raw/rao-2014-gm12878-mbol-1kb-mres-chr4-tads.json'
+  url: 'https://gist.githubusercontent.com/flekschas/37b6293b82b5b3e5fb56de9827100797/raw/9d6f05d5b4dc783560337772a203d66f30a068aa/rao-2014-gm12878-mbol-1kb-mres-chr4-tads-hipiler-v140.json'
 };
 
 export const rao2014Gm12878Mbol1kbMresTelomeres = {
   name: 'Telomere contacts of GM2878',
   data: 'Rao et al. (2014) GM12878 Mbol 1kb',
-  url: 'https://rawgit.com/flekschas/3700ffa02aac69b52a2b10300299f967/raw/rao-2014-gm12878-mbol-1kb-mres-telomeres.json'
+  url: 'https://gist.githubusercontent.com/flekschas/3700ffa02aac69b52a2b10300299f967/raw/f0b93a08d0b3f2d7c7a6bcca31b502f1234d235e/rao-2014-gm12878-mbol-1kb-mres-telomeres-hipiler-v140.json'
 };
 
 export const rao2014Gm12878VsK562Mbol1kbMresTelomeres = {
   name: 'Telomere contacts of GM12878 vs K562',
   data: 'Rao et al. (2014) GM12878 and K562 Mbol 1kb',
-  url: 'https://rawgit.com/flekschas/a397dded3694fc3984c6acdae6a837fd/raw/rao-2014-gm12878-vs-k562-mbol-1kb-mres-telomeres.json'
+  url: 'https://gist.githubusercontent.com/flekschas/a397dded3694fc3984c6acdae6a837fd/raw/021bd894b0d84df8df05e65b442669b39f17b7c7/rao-2014-gm12878-vs-k562-mbol-1kb-mres-telomeres-hipiler-v140.json'
 };
 
 export default [
